c56ab9e20da427b4aaebec0be67a03e360dc3736
meifangchen <meifang417@gmail.com>
2019-02-19 00:19:54 -0500

    Add data retrieved from database for Shopping List Screen

---
+++ Firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+//const settings = {timestampsInSnapshots: true};
+
+const config = {
+  
+    apiKey: "AIzaSyCWhfnv5YGVh89Oxc26WilGu2kI9NET6ww",
+    authDomain: "foodmanager-4af3d.firebaseapp.com",
+    databaseURL: "https://foodmanager-4af3d.firebaseio.com",
+    projectId: "foodmanager-4af3d",
+    storageBucket: "foodmanager-4af3d.appspot.com",
+    messagingSenderId: "41696452489"
+};
+firebase.initializeApp(config);
+
+//firebase.firestore().settings(settings);
+
+export default firebase;
--- screens/ShoppingListScreen.js
+++ screens/ShoppingListScreen.js
@@ -1,42 +1,76 @@
-import React from 'react';
-import { ScrollView, Text, View, StyleSheet} from 'react-native';
-import { Button } from 'react-native-elements';
-import ShoppingItemInput from "../components/ShoppingItemInput";
-import ShoppingItemList from "../components/ShoppingItemList";
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, ActivityIndicator, View, Text } from 'react-native';
+import { List, ListItem, Button, Icon } from 'react-native-elements';
+import firebase from '../Firebase';
 
-export default class ShoppingListScreen extends React.Component {
-
+class ShoppingListScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: 'Shopping List',
       headerRight: (
         <Button
-          title="Add"
           buttonStyle={{ padding: 0, backgroundColor: 'transparent' }}
           icon={{ name: 'add-circle', style: { marginRight: 0, fontSize: 28 } }}
-         // onPress={() => { navigation.push('AddBoard') }}
+          onPress={() => { navigation.push('AddBoard') }}
         />
       ),
     };
   };
-
-  state = {
-    shoppingItems: []
-  };
-
-  shoppingItemAddedHandler = shoppingItemName => {
-    this.setState(prevState => {
-      return {
-        shoppingItems: prevState.shoppingItems.concat(shoppingItemName)
-      };
+  constructor() {
+    super();
+    this.ref = firebase.firestore().collection('boards');
+    this.unsubscribe = null;
+    this.state = {
+      isLoading: true,
+      boards: []
+    };
+  }
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
+  onCollectionUpdate = (querySnapshot) => {
+    const boards = [];
+    querySnapshot.forEach((doc) => {
+      const { title, description, author } = doc.data();
+      boards.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        title,
+        description,
+        author,
+      });
     });
-  };
-
+    this.setState({
+      boards,
+      isLoading: false,
+   });
+  }
   render() {
+    if(this.state.isLoading){
+      return(
+        <View style={styles.activity}>
+          <ActivityIndicator size="large" color="#0000ff"/>
+        </View>
+      )
+    }
     return (
       <ScrollView style={styles.container}>
-      <ShoppingItemInput onshoppingItemAdded={this.shoppingItemAddedHandler} />
-      <ShoppingItemList shoppingItems={this.state.shoppingItems} />
+        <List>
+          {
+            this.state.boards.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{name: 'book', type: 'font-awesome'}}
+                onPress={() => {
+                  this.props.navigation.navigate('BoardDetails', {
+                    boardkey: `${JSON.stringify(item.key)}`,
+                  });
+                }}
+              />
+            ))
+          }
+        </List>
       </ScrollView>
     );
   }
@@ -44,7 +78,25 @@ export default class ShoppingListScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ecf0f1',
+   flex: 1,
+   paddingBottom: 22
   },
-});
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  activity: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default ShoppingListScreen;
+
+
