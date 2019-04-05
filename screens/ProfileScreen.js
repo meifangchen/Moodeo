c53b4d0e3dc9ec90313aaa60def6d91cf33f0b07
meifangchen <meifang417@gmail.com>
2019-04-05 00:27:05 -0500

    Implement UI for ProfileScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { ScrollView, StyleSheet} from 'react-native';
+import { ActivityIndicator, StyleSheet, ScrollView, View, Text } from 'react-native';
 import { List, ListItem, Button, Icon } from 'react-native-elements';
 import firebase from '../Firebase';
+import { AuthInput } from '../components/AuthInput';
+import { AuthButton } from '../components/AuthButton';
 
 export default class ProfileScreen extends React.Component {
 
@@ -18,9 +20,70 @@ export default class ProfileScreen extends React.Component {
     };
   };
 
+  state = {
+    gender: '',
+    height: 0,
+    weight: 0,
+    age: 0,
+  }
+
+  onPressSave() {  
+    const { gender, height, weight, age } = this.state;
+    alert("Your edited profile has been saved!");
+  }
+
+  onPressCancel() {
+    this.setState({
+      gender: '',
+      height: 0,
+      weight: 0,
+      age: 0,
+      message: ''
+    });
+  }
+
+  renderCurrentState() {
+    return (
+      <View style={styles.container}>
+        <AuthInput
+          placeholder=''
+          label='Gender:(F or M)'
+          onChangeText={gender => this.setState({ gender })}
+          value={this.state.gender}
+        />
+        <AuthInput
+          placeholder=''
+          label='Height(In inches)'
+          secureTextEntry
+          onChangeText={height => this.setState({ height })}
+          value={this.state.height}
+        />
+          <AuthInput
+          placeholder=''
+          label='Weight:(In lbs)'
+          onChangeText={weight => this.setState({ weight })}
+          value={this.state.weight}
+        />
+        <AuthInput
+          placeholder=''
+          label='Age(In years)'
+          secureTextEntry
+          onChangeText={age => this.setState({ age })}
+          value={this.state.age}
+        />
+        <AuthButton onPress={() => this.onPressSave()}>Save</AuthButton>
+        <AuthButton onPress={() => this.onPressCancel()}>Cancel</AuthButton>
+      </View>
+    )
+
+  }
+
   render() {
     return (
       <ScrollView style={styles.container}>
+        <View style={styles.container}>
+          {this.renderCurrentState()}
+        </View>
       </ScrollView>
     );
   }
@@ -29,8 +92,10 @@ export default class ProfileScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
+    padding: 10,
+    paddingTop: 20,
     backgroundColor: '#fff',
+    flexDirection: 'column'
   },
 });
 
